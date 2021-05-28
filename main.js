5d92c040b3a8b7d09ef27904856b226899b24c20
Cargalher <cargalher@gmail.com>
2021-05-28 16:39:34 +0200

    starting bonus

--- main.js
+++ main.js
@@ -1,13 +1,20 @@
 const app = new Vue ({
     el: '#app',
     data:{
-        discs: []
+        discs: [],
+        genres: []
     },
     mounted(){
         axios.get('https://flynn.boolean.careers/exercises/api/array/music')                    //calling ajax
-        .then((response) => {                                                                   //saving all the discs
+        .then((response) => {   
+            // Mandatory_part stampiamo a schermo una card per ogni album.                     //saving all the discs
             this.discs = response.data.response;
-            // console.log(this.discs);
+            console.log(this.discs);
+            //bonus1_ Creare una select con tutti i generi dei dischi. In base a cosa scegliamo nella select, vedremo i corrispondenti cd.
+            this.discs.forEach(disk =>{
+                this.genres.push(disk.genre);
+                console.log(genres);
+            });
         })
     }
 })
