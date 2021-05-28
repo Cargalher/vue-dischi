04c091534d58ed3c2ab3005a33fe2cc3ec37b4a6
Cargalher <cargalher@gmail.com>
2021-05-28 12:53:13 +0200

    axios strated

--- main.js
+++ main.js
@@ -1,12 +1,17 @@
 const app = new Vue ({
     el: '#app',
     data:{
-
+        url: 'https://flynn.boolean.careers/exercises/api/array/music',
+        discs: []
     },
-    methods (){
+    methods: {
 
     },
     mounted(){
-        
+        axios.get(this.url)
+        .then(response =>{
+            console.log(response.data.response);
+            this.discs.push(response.data.response);
+        })
     }
 })
