5aea3ad185b7df2a7a9842efe136aa37ebd9c91a
Cargalher <cargalher@gmail.com>
2021-05-28 15:31:07 +0200

    discs' posters added

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ const app = new Vue ({
         axios.get('https://flynn.boolean.careers/exercises/api/array/music')                    //calling ajax
         .then((response) => {                                                                   //saving all the discs
             this.discs = response.data.response;
+            // console.log(this.discs);
         })
     }
 })
